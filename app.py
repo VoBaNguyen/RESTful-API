e728c719bb1d61b73d0be360bc56b79ccd3e1063
VoBaNguyen <vbnguyen.dhsp@gmail.com>
2021-06-28 22:47:16 +0700

    Fix bug postgresql url

--- app.py
+++ app.py
@@ -10,9 +10,11 @@ from resources.store import Store, StoreList
 
 
 app = Flask(__name__)
-app.config["SQLALCHEMY_DATABASE_URI"] = os.environ.get(
-    "DATABASE_URL", "sqlite:///data.db"
-)
+if os.environ.get("DATABASE_URL"):
+    db_url = re.sub("postgres", "postgresql", os.environ.get("DATABASE_URL"))
+else:
+    du_url = "sqlite:///data.db"
+app.config["SQLALCHEMY_DATABASE_URI"] = db_url
 app.config[
     "SQLALCHEMY_TRACK_MODIFICATIONS"
 ] = False  # Turn of flask SQLAlchemy modification tracker
